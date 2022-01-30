8beb1e0c6f9b7287acbfc604ea9a8948335e7f97
David Soto <davidsotosendarrubias@gmail.com>
2022-01-30 12:31:05 +0100

    Created htmk estructure and CardComponent to see the web structure

--- public/js/Components/Card.js
+++ public/js/Components/Card.js
@@ -1,12 +1,26 @@
-import Component from "./Component";
+import Component from "./Component.js";
 
 // eslint-disable-next-line no-unused-vars
 class PokemonCard extends Component {
-  pokmonID;
+  pokemon;
 
-  constructor(parentElement, htmlTag, pokemonID) {
+  constructor(parentElement, htmlTag, pokemon) {
     super(parentElement, "pokemonCard", htmlTag);
 
-    this.pokmonID = pokemonID;
+    this.pokemon = pokemon;
+    this.getDrawHtml();
+  }
+
+  getDrawHtml() {
+    this.element.innerHTML = `
+    <div>
+      <img src="${this.pokemon.sprites.front_default}" alt="">
+      <div>
+        <h3>ID: ${this.pokemon.id}</h3>
+        <h4>${this.pokemon.name}</h4>
+      </div>
+    </div>`;
   }
 }
+
+export default PokemonCard;
